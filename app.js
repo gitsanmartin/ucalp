80cb8cf004a8f7a569c5d2649013f25876401d67
Adrián San Martín <sanmartin.adrian@gmail.com>
2022-09-22 22:35:26 -0300

    init app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import cors from 'cors'
+import morgan from 'morgan'
+
+const PORT = process.env.PORT || 3000
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+app.use(morgan('dev'))
+
+app.listen(PORT, () => {
+	console.log(` ✅ Server UP running in PORT ${PORT} ❤️`)
+})

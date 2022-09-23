3def34e14d16fb4219927b437befc6304852d71a
Adrián San Martín <sanmartin.adrian@gmail.com>
2022-09-22 23:39:40 -0300

    add route classrooms & careers

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ucalp
 sistema web de gestión de aulas en un establecimiento
+
+# prisma migrate init
+npx prisma migrate dev --name init
--- app.js
+++ app.js
@@ -2,6 +2,9 @@ import express from 'express'
 import cors from 'cors'
 import morgan from 'morgan'
 
+import classroomsRoutes from './routes/classroom.routes.js'
+import careerRoutes from './routes/career.routes.js'
+
 const PORT = process.env.PORT || 3000
 
 const app = express()
@@ -9,6 +12,9 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 app.use(morgan('dev'))
+
+app.use('/classrooms', classroomsRoutes)
+app.use('/careers', careerRoutes)
 
 // app.listen(PORT, '0.0.0.0', () => {
 // 	console.log(` ✅ Server UP running in PORT ${PORT} ❤️`)

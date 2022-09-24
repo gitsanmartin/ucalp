6e46562adf89481ca21a3d4309458493caef0d21
Adrián San Martín <sanmartin.adrian@gmail.com>
2022-09-24 00:23:07 -0300

    simple handling subject

--- controllers/subjects.controller.js
+++ controllers/subjects.controller.js
@@ -21,87 +21,115 @@ export const getSubjects = async (req, res) => {
 				
 			}
 		})
-
 		res.json(subjects)
+
 	} catch (error) {
-		res.status(500).json({
-			message: "Algo anda mal"
-		})
+		res.status(500).json(error)
 	}
 }
 
 export const getSubject = async (req, res) => { 
 	const {id} = req.params
-	const subject = await prisma.subject.findUnique({
-		where: {
-			id: Number(id)
-		},
-		select: {
-			name: true,
-			split: true,
-			classroom: {
-				select: {
-					number: true,
-					capacity: true
+	try {
+		const subject = await prisma.subject.findUnique({
+			where: {
+				id: Number(id)
+			},
+			select: {
+				name: true,
+				split: true,
+				classroom: {
+					select: {
+						number: true,
+						capacity: true
+					}
 				}
 			}
-		}
-	})
-	res.json({subject})
+		})
+		res.json(subject)
+		
+	} catch (error) {
+		res.status(500).json(error)
+	}
 }
 
-export const createSubject = async (req, res) => { 
+export const createSubject = async (req, res) => {
 	const {nameSubject, duration, year} = req.body
-	const result = await prisma.subject.create({
-		data: {
-			nameSubject,
-			duration,
-			year
-		}
-	})
-	res.json(result)
+	try {
+		const subject = await prisma.subject.create({
+			data: {
+				nameSubject: nameSubject.toLowerCase(),
+				duration: duration.toLowerCase(),
+				year
+			}
+		})
+		res.json(subject)
+		
+	} catch (error) {
+		res.status(500).json(error)
+	}
 }
 
 export const updateSubject = async (req, res) => { 
 	const {id} = req.params
 	const {nameSubject, duration, year} = req.body
-	const subject = await prisma.subject.update({
-		where: {id: Number(id)},
-		data: {
-			nameSubject, 
-			duration,
-			year
-		}
-	})
-	res.json(subject)
+	try {
+		const subject = await prisma.subject.update({
+			where: {id: Number(id)},
+			data: {
+				nameSubject: nameSubject.toLowerCase(),
+				duration: duration.toLowerCase(),
+				year
+			}
+		})
+		res.json(subject)
+		
+	} catch (error) {
+		res.status(500).json(error)
+	}
 }
 
 export const deleteSubject = async (req, res) => { 
 	const {id} = req.params
-	const subject = await prisma.subject.delete({
-		where: {id: Number(id)}
-	})
-	res.json('Delete one subject')
+	try {
+		await prisma.subject.delete({
+			where: {id: Number(id)}
+		})
+		res.json('Delete one subject')
+
+	} catch (error) {
+		res.status(500).json(error)
+	}
 }
 
 export const setRelationProfessor = async (req, res) => {
 	const {subjectId, professorId} = req.body
-	const relation = await prisma.subjectAndProfessor.create({
-		data: {
-			subjectId,
-			professorID: professorId
-		}
-	})
-	res.json(relation)
+	try {
+		const relation = await prisma.subjectAndProfessor.create({
+			data: {
+				subjectId,
+				professorID: professorId
+			}
+		})
+		res.json(relation)
+		
+	} catch (error) {
+		res.status(500).json(error)
+	}
 }
 
 export const setRelationCareer = async (req, res) => {
 	const {subjectId, careerId} = req.body
-	const relation = await prisma.subjectAndCareer.create({
-		data: {
-			subjectId,
-			careerId
-		}
-	})
-	res.json(relation)
+	try {
+		const relation = await prisma.subjectAndCareer.create({
+			data: {
+				subjectId,
+				careerId
+			}
+		})
+		res.json(relation)
+		
+	} catch (error) {
+		res.status(500).json(error)
+	}
 }

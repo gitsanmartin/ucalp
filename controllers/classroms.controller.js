146f3537363a77debcc6a286c6d90d0f26553a16
Adrián San Martín <sanmartin.adrian@gmail.com>
2022-09-23 23:44:02 -0300

    simple handling and convert to lower case

--- controllers/classroms.controller.js
+++ controllers/classroms.controller.js
@@ -8,55 +8,73 @@ export const getClassrooms = async (req, res) => {
 				subjects: true
 			}
 		})
-
 		res.json(classrooms) // or {classrooms} for array[{classrooms}]
+
 	} catch (error) {
-		res.status(500).json({
-			message: "Algo anda mal"
-		})
+		res.status(500).json(error)
 	}
 }
 
 export const getClassroom = async (req, res) => { 
 	const {id} = req.params
-	const classroom = await prisma.classroom.findUnique({
-		where: {
-			id: Number(id)
-		}
-	})
-	res.json(classroom)
+	try {
+		const classroom = await prisma.classroom.findUnique({
+			where: {
+				id: Number(id)
+			}
+		})
+		res.json(classroom)
+		
+	} catch (error) {
+		res.status(500).json(error)
+	}
 }
 
 export const createClassroom = async (req, res) => { 
 	const {classroomNumber, description, capacity} = req.body
-	const result = await prisma.classroom.create({
-		data: {
-			classroomNumber,
-			description,
-			capacity
-		}
-	})
-	res.json(result)
+	try {
+		const result = await prisma.classroom.create({
+			data: {
+				classroomNumber,
+				description: description.toLowerCase(),
+				capacity
+			}
+		})
+		res.json(result)
+		
+	} catch (error) {
+		res.status(500).json(error)
+	}
 }
 
 export const updateClassroom = async (req, res) => { 
 	const {id} = req.params
 	const {classroomNumber, description, capacity} = req.body
-	const classroom = await prisma.classroom.update({
-		where: {id: Number(id)},
-		data: {
-			classroomNumber,
-			description,
-			capacity
-		}
-	})
-	res.json(classroom)
+	try {
+		const classroom = await prisma.classroom.update({
+			where: {id: Number(id)},
+			data: {
+				classroomNumber: classroomNumber.toLowerCase(),
+				description: description.toLowerCase(),
+				capacity: capacity.toLowerCase()
+			}
+		})
+		res.json(classroom)
+		
+	} catch (error) {
+		res.status(500).json(error)
+	}
 }
 
 export const deleteClassroom = async (req, res) => { 
 	const {id} = req.params
-	const classroom = await prisma.classroom.delete({
-		where: {id: Number(id)}
-	})
-	res.json('Delete classroom')
+	try {
+		await prisma.classroom.delete({
+			where: {id: Number(id)}	
+		})
+		res.json('Delete classroom')
+
+	} catch (error) {
+		res.status(500).json(error)
+	}
 }
